5bd311c82f3771218997d856f0d9d3a38a972caf
Reshma <reshmashaik0910@gmail.com>
2024-10-10 12:13:55 +0100

    Added loho img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
+import { LOGO_URL } from "../utils/constants";
 const Header = ()=>{
     return(
         <div className = "flex justify-between items-center border border-black">
             <div>
-                <img className='p-4' src="" alt="Logo_img" />
+                <img className='p-4' src={LOGO_URL} alt="Logo_img" />
             </div>
             <div className="p-4">
                 <ul className='flex font-bold'>
